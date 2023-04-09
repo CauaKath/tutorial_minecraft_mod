b2c4805c38092f9b8dbb99542a97bfe64c044551
Cauã Kath <80467897+CauaKath@users.noreply.github.com>
2023-04-09 11:59:04 -0300

    feat: added items

--- src/main/java/net/kath/medieval_rpg_for_dummies/MedievalRpgMod.java
+++ src/main/java/net/kath/medieval_rpg_for_dummies/MedievalRpgMod.java
@@ -1,6 +1,7 @@
 package net.kath.medieval_rpg_for_dummies;
 
 import com.mojang.logging.LogUtils;
+import net.kath.medieval_rpg_for_dummies.item.ModItems;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -20,6 +21,8 @@ public class MedievalRpgMod {
 
     public MedievalRpgMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
---
+++ src/main/java/net/kath/medieval_rpg_for_dummies/item/ModItems.java
@@ -0,0 +1,23 @@
+package net.kath.medieval_rpg_for_dummies.item;
+
+import net.kath.medieval_rpg_for_dummies.MedievalRpgMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+
+  public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MedievalRpgMod.MOD_ID);
+
+  public static final RegistryObject<Item> GOSLIM = ITEMS.register("goslim",
+          () -> new Item(new Item.Properties().tab(ModCreativeModeTab.MEDIEVAL_RPG_FOR_DUMMIES_TAB)));
+
+  public static final RegistryObject<Item> RAW_GOSLIM = ITEMS.register("raw_goslim",
+          () -> new Item(new Item.Properties().tab(ModCreativeModeTab.MEDIEVAL_RPG_FOR_DUMMIES_TAB)));
+
+  public static void register(IEventBus eventBus) {
+    ITEMS.register(eventBus);
+  }
+}
