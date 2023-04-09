56efce5a980a65c55ded6471dc2120df8016d37b
Cauã Kath <80467897+CauaKath@users.noreply.github.com>
2023-04-09 11:10:53 -0300

    Initial commit

---
+++ src/main/java/net/kath/medieval_rpg_for_dummies/MedievalRpgMod.java
@@ -0,0 +1,40 @@
+package net.kath.medieval_rpg_for_dummies;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+
+@Mod(MedievalRpgMod.MOD_ID)
+public class MedievalRpgMod {
+
+    public static final String MOD_ID = "medieval_rpg_for_dummies";
+
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public MedievalRpgMod() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
